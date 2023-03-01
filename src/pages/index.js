cc7737535580dc283f5f658a8d126f93eafe211e
evabanegacom <udegbue69@gmail.com>
2023-03-01 19:15:33 +0100

    started api implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,55 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Input, List, Pagination } from 'antd';
+import { useState, useEffect } from "react";
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const PAGE_SIZE = 25;
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const url = `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${PAGE_SIZE}&q=${searchTerm}`;
+      console.log({url})
+      const response = await fetch(url);
+      const data = await response.json();
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, [currentPage, searchTerm]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const url = `https://jsonplaceholder.typicode.com/users?q=${searchTerm}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, [searchTerm]);
+
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
+
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value.toLowerCase());
+  }
+
+  console.log({searchTerm})
+  console.log({searchTerm})
+
+  const filteredPosts = posts.filter((post) => {
+    const user = users.find((user) => user.id === post.userId);
+    return user && user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  });
+
   return (
     <>
       <Head>
@@ -15,107 +59,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <div>
+          <Input placeholder="Search by user name" value={searchTerm} onChange={handleSearchChange} />
+          <List
+            dataSource={filteredPosts}
+            renderItem={(post) => {
+              const user = users.find((user) => user.id === post.userId);
+              if (!user) {
+                return null;
+              }
+              return (
+                <List.Item>
+                  <List.Item.Meta title={post.title} description={post.body} />
+                  <div>{user.name} ({user.username})</div>
+                </List.Item>
+              );
+            }}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <Pagination
+            current={currentPage}
+            total={100}
+            pageSize={PAGE_SIZE}
+            onChange={handlePageChange}
+          />
         </div>
       </main>
     </>
